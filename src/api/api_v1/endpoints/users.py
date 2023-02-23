4ee334ed072f8abcd17eddc078d088883e798a44
nstukalov <nstukalov@apptrix.ru>
2023-02-23 11:08:10 +0300

    added routing

---
+++ src/api/api_v1/endpoints/users.py
@@ -0,0 +1,15 @@
+from typing import Dict, List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import crud, schemas
+from src.api import deps
+
+router = APIRouter()
+
+
+@router.get("", response_model=List[schemas.UserOut])
+async def get_users(session: AsyncSession = Depends(deps.get_session)) -> Dict:
+    users = await crud.user.get_all_users(session)
+    return users
--- src/main.py
+++ src/main.py
@@ -1,27 +1,19 @@
-from typing import Dict, List
-
 import uvicorn
-from fastapi import Depends, FastAPI
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import FastAPI
 
-from src import crud, schemas
-from src.api import deps
+from src.api.api_v1.api import api_router
 from src.core.config import settings
 from src.db.init_db import init_db
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 
-@app.get("/users", response_model=List[schemas.UserOut])
-async def get_users(session: AsyncSession = Depends(deps.get_session)) -> Dict:
-    users = await crud.user.get_all_users(session)
-    return users
-
-
 @app.on_event("startup")
 async def startup():
     await init_db()
 
+
+app.include_router(api_router, prefix=settings.API_PREFIX_V1)
 
 if __name__ == "__main__":
     uvicorn.run(app)
