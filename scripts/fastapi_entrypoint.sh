6a3d183944b0f514234059afa62304080210d913
Nikolai-Zinchenko <nikolaizinchenko94@gmail.com>
2023-02-26 12:14:46 +0300

    test pipeline

--- scripts/fastapi_entrypoint.sh
+++ scripts/fastapi_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 alembic upgrade head
 uvicorn src.main:app --host 0.0.0.0 --port 8000
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,9 +2,13 @@
 
 # Exit in case of error
 set -e
+export COMPOSE_PROJECT_NAME=test_twitter
 
-docker-compose -f docker-compose.tests.yml build
-docker-compose -f docker-compose.tests.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-compose.tests.yml up -d
-docker-compose -f docker-compose.tests.yml exec -T test_app pytest
-docker-compose -f docker-compose.tests.yml down -v --remove-orphans
+sed -i 's/8000:8000/8888:8000/g' docker-compose.yml
+
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml down -v --remove-orphans
+docker-compose -f docker-compose.yml up -d
+docker exec -i test_twitter_db_1 psql -U ${POSTGRES_USER} postgres -c "CREATE DATABASE ${TEST_POSTGRES_DB}"
+docker exec -i test_twitter_app_1 pytest
+docker-compose -f docker-compose.yml down -v --remove-orphans
