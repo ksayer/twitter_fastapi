3dd4a6c0ac46d6d1f8119baab6d40023ae90aadb
zinchenko-apptrix <zinchenko@apptrix.ru>
2023-03-08 19:48:22 +0300

    added m2m mediatwit

--- src/models/media.py
+++ src/models/media.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship  # type: ignore
 from src.db.base_class import Base
 
 if TYPE_CHECKING:
+    from .twit import Twit
     from .user import User
 
 
@@ -13,6 +14,22 @@ class Media(Base):
     file: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     user: Mapped['User'] = relationship(back_populates='media')  # type: ignore
+    twits: Mapped['MediaTwit'] = relationship(  # type: ignore
+        back_populates='media',
+        cascade="all, delete-orphan",
+    )
 
     def __repr__(self):
         return self.file
+
+
+class MediaTwit(Base):
+    id = None  # type: ignore
+    media_id: Mapped[int] = mapped_column(
+        ForeignKey('media.id'), nullable=False, primary_key=True, index=True
+    )
+    twit_id: Mapped[int] = mapped_column(
+        ForeignKey('twit.id'), nullable=False, primary_key=True, index=True
+    )
+    media: Mapped[Media] = relationship(back_populates='twits')  # type: ignore
+    twit: Mapped['Twit'] = relationship(back_populates='media')  # type: ignore
--- src/models/twit.py
+++ src/models/twit.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship  # type: ignore
 from src.db.base_class import Base
 
 if TYPE_CHECKING:
+    from .media import MediaTwit
     from .user import User
 
 
@@ -13,6 +14,9 @@ class Twit(Base):
     content: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     user: Mapped['User'] = relationship(back_populates='twits')  # type: ignore
+    media: Mapped['MediaTwit'] = relationship(  # type: ignore
+        back_populates='twit', cascade="all, delete-orphan"
+    )
 
     def repr(self) -> str:
         return 'Twit ID={twit_id} of {user}'.format(
