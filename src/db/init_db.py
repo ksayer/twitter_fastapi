34572b888140a1e3e014f62087da997521703c0e
ZinchenkoNikolai <zinchenko@apptrix.ru>
2023-03-19 16:54:03 +0300

    fix static path

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -7,9 +7,10 @@ from src.db.session import session as async_session
 async def init_db() -> None:
     user = User(name='Nikolai', key='qwerty')
     user2 = User(name='Kate', key='ytrewq')
+    user3 = User(name='Elon', key='test')
     async with async_session() as sesion:
         async with sesion.begin():
             users = await sesion.execute(select(User))
             if not users.scalars().unique().all():
-                sesion.add_all([user, user2])
+                sesion.add_all([user, user2, user3])
                 await sesion.commit()
